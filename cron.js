a4f4efa644a8284f8a6aa5a6d473c94646bc3612
BradleyHazel <bradleyhazel@gmail.com>
2022-07-20 20:17:04 -0400

    deploy

--- cron.js
+++ cron.js
@@ -5,7 +5,11 @@ Knight.updateMany({},
     {completedToday:false}, function (err, docs) {
     if (err){
         console.log(err)
+        process.exit()
     }
     else{
         console.log("Updated Docs : ", docs);
+        process.exit()
     }})
+
+ 
